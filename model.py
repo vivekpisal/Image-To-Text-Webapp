bee66da189b4ff50b7f198bc7859ea266837ab82
vivekpisal <54746512+vivekpisal@users.noreply.github.com>
2020-07-13 19:20:29 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,14 +1,20 @@
 import cv2
-import easyocr
+#import easyocr
 from pylab import rcParams
 from IPython.display import Image
+import pytesseract
 
 
 rcParams['figure.figsize']=8,16
 
-
+'''
 def ImageToText(n):
 	reader=easyocr.Reader(['en'])
 	Image(n)
 	op=reader.readtext(n)
+	return op'''
+
+def ImageToText(n):
+	img=cv2.imread(n)
+	op=pytesseract.image_to_string(img)
 	return op
